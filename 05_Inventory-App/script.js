98a56d666f3d59375d5810f190b51d5d9c0be3d1
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-12 11:53:32 +0100

    inventory app

--- 05_Inventory-App/script.js
+++ 05_Inventory-App/script.js
@@ -6,8 +6,18 @@ const inventory = JSON.parse(localStorage.getItem('inventory')) || [];
 function addInventory (e) {
     e.preventDefault ();
 
+    let type = document.getElementById('type').value;
     let title = document.getElementById('inventory-title').value;
     let amount = document.getElementById('user-amount').value;
 
-    if ( ty)
+    if ( type !='chooseOne'
+    && title.length > 0
+    && amount > 0) {
+        const inventory = {
+            type,
+            title,
+            amount,
+            id: inventory.length > 0 ? inventory[inventory.length - 1].id + 1 : 1,
+        }
+    }
 }
