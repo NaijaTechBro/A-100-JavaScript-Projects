0af326681f9c1c6305cb35fdf62bc920ba46a53f
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-01-12 11:41:48 +0100

    inventory app

--- 05_Inventory-App/script.js
+++ 05_Inventory-App/script.js
@@ -1,1 +1,10 @@
-const ProductTitleError = document.getElementById('product-title-error')
+
+
+const inventory = JSON.parse(localStorage.getItem('inventory')) || [];
+
+function addInventory (e) {
+    e.preventDefault ();
+
+    let title = document.getElementById('inventory-title').value;
+    let amount = document.getElementById('user-amount').value;
+}
